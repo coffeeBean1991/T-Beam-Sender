8167ec97638ba6465d0ab4b47a9415b58c08d85a
Arina Moraes <arinamoraes@Arinas-MBP.hitronhub.home>
2020-11-12 21:39:15 -0500

    clean up and id transfer via lora

--- T-Beam-Sender/T-Beam-Sender.ino
+++ T-Beam-Sender/T-Beam-Sender.ino
@@ -3,15 +3,15 @@
 #include <Wire.h>
 #include "SSD1306.h"
 #include <TinyGPS++.h>
-#include <axp20x.h>
-AXP20X_Class axp;
+//#include <axp20x.h>
+//AXP20X_Class axp;
 #define SCK     5    // GPIO5  -- SX1278's SCK
 #define MISO    19   // GPIO19 -- SX1278's MISnO
 #define MOSI    27   // GPIO27 -- SX1278's MOSI
 #define SS      18   // GPIO18 -- SX1278's CS
 #define RST     14   // GPIO14 -- SX1278's RESET
 #define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
-#define BAND  868E6
+#define BAND  915E6
 
 SSD1306 display(0x3c, 21, 22);
 String rssi = "RSSI --";
@@ -23,30 +23,15 @@ TinyGPSPlus gps;
 HardwareSerial GPS(1);
 
 char ID[23];
-void setup() {
+void setup()
+{
   uint64_t chipid = ESP.getEfuseMac(); // The chip ID is essentially its MAC address(length: 6 bytes).
   uint16_t chip = (uint16_t)(chipid >> 32);
-
   snprintf(ID, 23, "IAT-%04X%08X", chip, (uint32_t)chipid);
-  Wire.begin(21, 22);
-  if (!axp.begin(Wire, AXP192_SLAVE_ADDRESS)) {
-    Serial.println("AXP192 Begin PASS");
-  } else {
-    Serial.println("AXP192 Begin FAIL");
-  }
-  axp.setPowerOutPut(AXP192_LDO2, AXP202_ON); //LORA
-  axp.setPowerOutPut(AXP192_LDO3, AXP202_ON); //GPS
-  axp.setPowerOutPut(AXP192_DCDC2, AXP202_ON);
-  axp.setPowerOutPut(AXP192_EXTEN, AXP202_ON);
-  axp.setPowerOutPut(AXP192_DCDC1, AXP202_ON); //OLED
-  axp.setChgLEDMode(AXP20X_LED_LOW_LEVEL);
-  GPS.begin(9600, SERIAL_8N1, 34, 12);   //17-TX 18-RX
-  pinMode(16, OUTPUT);
-  pinMode(2, OUTPUT);
 
-  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
-  delay(50);
-  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
+
+  GPS.begin(9600, SERIAL_8N1, 34, 12);   //17-TX 18-RX
+
 
   Serial.begin(115200);
   while (!Serial);
@@ -55,7 +40,7 @@ void setup() {
 
   SPI.begin(SCK, MISO, MOSI, SS);
   LoRa.setPins(SS, RST, DI0);
-  if (!LoRa.begin(868E6)) {
+  if (!LoRa.begin(BAND)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
@@ -64,12 +49,6 @@ void setup() {
   display.init();
   display.flipScreenVertically();
   display.setFont(ArialMT_Plain_10);
-  Wire.begin(21, 22);
-  if (!axp.begin(Wire, AXP192_SLAVE_ADDRESS)) {
-    Serial.println("AXP192 Begin PASS");
-  } else {
-    Serial.println("AXP192 Begin FAIL");
-  }
 
   delay(1500);
 }
@@ -81,10 +60,11 @@ void loop()
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.setFont(ArialMT_Plain_10);
   display.drawString(0, 0, "Sending packet");
-  display.drawString(0, 10, "Latitude:");
-  display.drawString(50, 10, String(gps.location.lat(), 5));
-  display.drawString(0, 20, "Longitude:");
-  display.drawString(50, 20, String(gps.location.lng(), 5));
+  display.drawString(20, 15, String(ID));
+  display.drawString(0, 30, "Latitude:");
+  display.drawString(55, 30, String(gps.location.lat(), 5));
+  display.drawString(0, 40, "Longitude:");
+  display.drawString(55, 40, String(gps.location.lng(), 5));
   display.display();
 
   //Build timestamp
@@ -102,14 +82,14 @@ void loop()
   // send packet
   LoRa.beginPacket();
   LoRa.print(ID);
+  LoRa.print(";");
   LoRa.print(gps.location.lat(), 5);
   LoRa.print(";");
   LoRa.print(gps.location.lng(), 5);
   LoRa.print(";");
   LoRa.print(unixtime);
   LoRa.endPacket();
-  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
+
   Serial.print("Latitude  : ");
   Serial.println(gps.location.lat(), 5);
   Serial.print("Longitude : ");
